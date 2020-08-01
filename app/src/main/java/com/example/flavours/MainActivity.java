6f49eb0c19b4201dff30e12ce19cc29fbc72f02f
Afshan Jabeen <afshanaffu26@gmail.com>
2020-08-01 17:46:53 -0400

    splash and login screen

--- app/src/main/java/com/example/flavours/MainActivity.java
+++ app/src/main/java/com/example/flavours/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.flavours;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
-
+    Window window;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(Build.VERSION.SDK_INT>=21){
+            window=this.getWindow();
+            window.setStatusBarColor(this.getResources().getColor(R.color.colorPrimary));
+        }
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                final Intent mainIntent =  new Intent(MainActivity.this, LoginActivity.class);
+                MainActivity.this.startActivity(mainIntent);
+                MainActivity.this.finish();
+            }
+        },3000);
     }
 }
