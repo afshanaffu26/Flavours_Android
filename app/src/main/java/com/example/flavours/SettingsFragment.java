b2ea12b02adf0ab2befc00bf6451f8ed6fbf9c02
Saikrishna Samudrala <saishetty26.sk@gmail.com>
2020-08-02 17:38:47 -0400

    Settings and Profile UI and Navigations

--- app/src/main/java/com/example/flavours/SettingsFragment.java
+++ app/src/main/java/com/example/flavours/SettingsFragment.java
@@ -1,5 +1,7 @@
 package com.example.flavours;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,13 +9,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link SettingsFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class SettingsFragment extends Fragment {
+public class SettingsFragment extends Fragment implements View.OnClickListener {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -59,6 +62,28 @@ public class SettingsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        // return inflater.inflate(R.layout.fragment_settings, container, false);
+        View v = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        ImageView imgProfileBtn = v.findViewById(R.id.imgProfileBtn);
+        imgProfileBtn.setOnClickListener(this);
+        ImageView imgHelplineBtn = v.findViewById(R.id.imgHelplineBtn);
+        imgHelplineBtn.setOnClickListener(this);
+        return v;
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.imgProfileBtn:
+                Intent i = new Intent(getActivity(), ProfileActivity.class);
+                startActivity(i);
+                ((Activity) getActivity()).overridePendingTransition(0, 0);
+                break;
+            case R.id.imgHelplineBtn:
+//                Intent intent = new Intent(getActivity(), HelplineActivity.class);
+//                startActivity(intent);
+//                ((Activity) getActivity()).overridePendingTransition(0, 0);
+                break;
+        }
     }
 }
