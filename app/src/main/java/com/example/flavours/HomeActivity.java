187a4935b19de082840dfb198e071e86495e241c
Akshit Moturi <akshit.moturi.gupta@gmail.com>
2020-08-02 11:34:32 -0400

    Home and cart fragments

--- app/src/main/java/com/example/flavours/HomeActivity.java
+++ app/src/main/java/com/example/flavours/HomeActivity.java
@@ -4,8 +4,12 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
@@ -22,6 +26,9 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
     private NavigationView navigationView;
     private BottomNavigationView mMainNav;
     private FrameLayout mMainFrame;
+    private HomeFragment homeFragment;
+    private CartFragment cartFragment;
+
 
     @Override
     public void onResume() {
@@ -49,6 +56,10 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         mMainFrame=findViewById(R.id.main_frame);
         mMainNav=findViewById(R.id.main_nav);
 
+        homeFragment = new HomeFragment();
+        cartFragment = new CartFragment();
+        setFragment(homeFragment);
+
         ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(
                 this,
                 drawerLayout,
@@ -66,9 +77,47 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
             }
         });
     }
+    private void setFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction= getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_frame,fragment);
+        fragmentTransaction.commit();
 
+    }
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        return false;
+        int id=item.getItemId();
+        switch(id){
+            case R.id.menu1cart:
+//                if (!isInFront)
+//                startActivity(new Intent(getApplicationContext(),HomeActivity.class));
+//                else
+//                    drawerLayout.closeDrawer(GravityCompat.START);
+                setFragment(cartFragment);
+                drawerLayout.closeDrawer(GravityCompat.START);
+                break;
+            case R.id.menu1history:
+                drawerLayout.closeDrawer(GravityCompat.START);
+                break;
+            case R.id.menu1offers:
+                drawerLayout.closeDrawer(GravityCompat.START);
+                break;
+            case R.id.menu2share:
+                drawerLayout.closeDrawer(GravityCompat.START);
+                break;
+            case R.id.menu2feedback:
+                drawerLayout.closeDrawer(GravityCompat.START);
+                break;
+            case R.id.menu2help:
+                drawerLayout.closeDrawer(GravityCompat.START);
+                break;
+            case R.id.menu3logout:
+                userLogout();
+                break;
+        }
+        return true;
+    }
+    private void userLogout() {
+        Intent i= new Intent(this,LoginActivity.class);
+        startActivity(i);
     }
 }
