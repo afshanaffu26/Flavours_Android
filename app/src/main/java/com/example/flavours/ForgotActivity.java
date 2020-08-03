85d0a87b05787863c9b0eaf9320aa794dae8d1ef
prasanth bavireddy <bavireddyprasanth@gmail.com>
2020-08-03 19:22:59 -0400

    added navigation from login page

--- app/src/main/java/com/example/flavours/ForgotActivity.java
+++ app/src/main/java/com/example/flavours/ForgotActivity.java
@@ -3,9 +3,12 @@ package com.example.flavours;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class ForgotActivity extends AppCompatActivity {
-
+public class ForgotActivity extends AppCompatActivity{
+    Button btnSubmit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/example/flavours/LoginActivity.java
+++ app/src/main/java/com/example/flavours/LoginActivity.java
@@ -7,10 +7,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button btnLogin;
+    TextView txt_recover_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +25,19 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
         btnLogin = findViewById(R.id.btnLogin);
         btnLogin.setOnClickListener(this);
+        txt_recover_password= findViewById(R.id.txt_recover_password);
+        txt_recover_password.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-        switch(view.getId()){
+        switch (view.getId()) {
             case R.id.btnLogin:
-                startActivity(new Intent(getApplicationContext(),HomeActivity.class));
+                startActivity(new Intent(getApplicationContext(), HomeActivity.class));
                 break;
+            case R.id.txt_recover_password:
+                startActivity(new Intent(getApplicationContext(), ForgotActivity.class));
+            }
         }
-    }
-}
+            }
+
