b71ed428c03e57b9c97035581d8c1ebdadea1976
prasanth bavireddy <bavireddyprasanth@gmail.com>
2020-08-04 01:04:02 -0400

    added faq, updated terms and conditions

---
+++ app/src/main/java/com/example/flavours/FaqActivity.java
@@ -0,0 +1,16 @@
+package com.example.flavours;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class FaqActivity extends AppCompatActivity {
+TextView Q1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_faq);
+
+    }
+}
