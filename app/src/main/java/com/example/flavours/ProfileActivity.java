93a97126b8a50ac559cdaf6d1377ff4257cb9304
Saikrishna Samudrala <saishetty26.sk@gmail.com>
2020-08-04 16:56:22 -0400

    Ui changes for smaller screen resoltuion

--- app/src/main/java/com/example/flavours/ProfileActivity.java
+++ app/src/main/java/com/example/flavours/ProfileActivity.java
@@ -1,14 +1,25 @@
 package com.example.flavours;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class ProfileActivity extends AppCompatActivity {
-
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return true;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Profile");
+        //display back button
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
