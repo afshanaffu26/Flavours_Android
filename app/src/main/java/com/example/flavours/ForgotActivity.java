0f79b5e130e3cd8a5b90cb3d749c69fb5268b761
prasanth bavireddy <bavireddyprasanth@gmail.com>
2020-08-03 20:13:06 -0400

    added toast message on clicking submit in recover password page

--- app/src/main/java/com/example/flavours/ForgotActivity.java
+++ app/src/main/java/com/example/flavours/ForgotActivity.java
@@ -7,11 +7,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class ForgotActivity extends AppCompatActivity{
+public class ForgotActivity extends AppCompatActivity implements View.OnClickListener{
     Button btnSubmit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot);
+        btnSubmit = findViewById(R.id.btnSubmit);
+        btnSubmit.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Toast.makeText(getApplicationContext(),"Password reset link has been sent to your Email", Toast.LENGTH_SHORT).show();
     }
 }
