511eb3103246e2e9c9352b9aae9b5243d6226393
Saikrishna Samudrala <saishetty26.sk@gmail.com>
2020-08-04 18:20:53 -0400

    added ui for item details screen

--- app/src/main/java/com/example/flavours/ItemDescriptionActivity.java
+++ app/src/main/java/com/example/flavours/ItemDescriptionActivity.java
@@ -4,8 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class ItemDescriptionActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class ItemDescriptionActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    private static final int MAX_QUANTITY = 20;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +24,35 @@ public class ItemDescriptionActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Flavours");
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+
+        // Spinner click listener
+        spinner.setOnItemSelectedListener(this);
+
+        // Spinner Drop down elements
+        List<Integer> categories = new ArrayList<>();
+        for (int i=1;i<=MAX_QUANTITY;i++){
+            categories.add(i);
+        }
+
+        // Creating adapter for spinner
+        ArrayAdapter<Integer> dataAdapter = new ArrayAdapter<Integer>(this, android.R.layout.simple_spinner_dropdown_item, categories);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        spinner.setAdapter(dataAdapter);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
