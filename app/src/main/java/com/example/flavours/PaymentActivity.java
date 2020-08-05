b87f1c89cf95a718de22554754bbe36d459c161e
prasanth bavireddy <bavireddyprasanth@gmail.com>
2020-08-04 20:57:48 -0400

    added navigations and payment page

--- app/src/main/java/com/example/flavours/PaymentActivity.java
+++ app/src/main/java/com/example/flavours/PaymentActivity.java
@@ -3,12 +3,21 @@ package com.example.flavours;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class PaymentActivity extends AppCompatActivity {
-
+public class PaymentActivity extends AppCompatActivity implements View.OnClickListener{
+Button btnPay;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
+        btnPay = findViewById(R.id.btnPay);
+        btnPay.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View view) {
+        Toast.makeText(getApplicationContext(),"Payment is being Processed, Please don't refresh", Toast.LENGTH_SHORT).show();
     }
 }
