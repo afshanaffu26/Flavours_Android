87d92d636fccb4387b65ab6e6203eb09fa3c4301
SURYA123-bit <surya.pasumarthi111@gmail.com>
2020-08-27 23:22:10 -0400

    Added Forget activity

--- app/src/main/java/com/example/flavours/ForgotActivity.java
+++ app/src/main/java/com/example/flavours/ForgotActivity.java
@@ -1,15 +1,26 @@
 package com.example.flavours;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ForgotActivity extends AppCompatActivity implements View.OnClickListener{
     Button btnSubmit;
+    FirebaseAuth mAuth;
+    EditText editEmail;
+    ProgressBar progressBar;
     @Override
     public boolean onSupportNavigateUp() {
         finish();
@@ -27,10 +38,48 @@ public class ForgotActivity extends AppCompatActivity implements View.OnClickLis
         getSupportActionBar().setTitle("Flavours");
         //display back button
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mAuth=FirebaseAuth.getInstance();
+        editEmail=(EditText)findViewById(R.id.editEmail);
+        progressBar=findViewById(R.id.progressbar);
     }
 
     @Override
     public void onClick(View view) {
-        Toast.makeText(getApplicationContext(),"Password reset link has been sent to your Email", Toast.LENGTH_SHORT).show();
+        switch(view.getId()){
+            case R.id.btnSubmit:
+                this.submitEmail();
+                break;
+        }
+    }
+    public void submitEmail() {
+        String email = editEmail.getText().toString().trim();
+
+        if(email.isEmpty())
+        {
+            editEmail.setError("Email is required");
+            editEmail.requestFocus();
+            return;
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            editEmail.setError("Please enter valid email");
+            editEmail.requestFocus();
+            return;
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+        mAuth.sendPasswordResetEmail(editEmail.getText().toString()).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                progressBar.setVisibility(View.GONE);
+                if (task.isSuccessful()){
+                    Toast.makeText(getApplicationContext(),"Password reset link is sent to your email",Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(getApplicationContext(),task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
